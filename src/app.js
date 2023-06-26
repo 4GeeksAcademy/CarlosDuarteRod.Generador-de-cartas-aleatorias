83a377642d6ee6288f66c155d5cf414966799873
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-06-26 20:37:02 +0000

    Proyecto final

--- src/app.js
+++ src/app.js
@@ -12,23 +12,21 @@ window.onload = function() {
   let simbolos2 = document.querySelector(".simbolos2");
 
 
-  let palos = ["spade", "club", "heart", "diamond"];
   let numeros = ["2","3","4","5","6","7","8","9","10","J","A","Q","K"];
   let arraySimbolos = ["♦","♥","♠","♣"];
 
   let numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
-  let palosaleatorio = palos[Math.floor(Math.random() * palos.length)];
-
-  if (palosaleatorio === "spade") simbolos.innerHTML = arraySimbolos[2];
-  if (palosaleatorio === "spade") simbolos2.innerHTML = arraySimbolos[2];
-  if (palosaleatorio === "club") simbolos.innerHTML = arraySimbolos[3];
-  if (palosaleatorio === "club") simbolos2.innerHTML = arraySimbolos[3];
-  if (palosaleatorio === "heart") simbolos.innerHTML = arraySimbolos[1];
-  if (palosaleatorio === "heart") simbolos2.innerHTML = arraySimbolos[1];
-  if (palosaleatorio === "diamond") simbolos.innerHTML = arraySimbolos[0];
-  if (palosaleatorio === "diamond") simbolos2.innerHTML = arraySimbolos[0];
-  
+  let arrayaleatorio = arraySimbolos[Math.floor(Math.random() * arraySimbolos.length)];
 
 
-  return (palosaleatorio, numeroAleatorio);
+  simbolos.innerHTML = arrayaleatorio;
+  simbolos2.innerHTML = arrayaleatorio;
+  numeroIndex.innerHTML = numeroAleatorio;
+
+  color.classList.remove("red","black");
+  if(arrayaleatorio === "♦" || arrayaleatorio === "♥") 
+    color.classList.add("red");
+  else color.classList.add("black");
+
+  return (arrayaleatorio, numeroAleatorio);
 };
